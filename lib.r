be084230df50d23139284347e7b4a9cccb02a133
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2023-12-22 11:30:15 +0200

    Add utility functions

--- lib.r
+++ lib.r
@@ -1,5 +1,24 @@
 # FUNCTIONS
 
+RESOURCE_PATH = "output"
+
+# Utility function to get a relative file path (including file extension)
+# from a file name.
+filepath_png <- function(name) {
+  return(file.path(RESOURCE_PATH, paste(name, ".png", sep = "")))
+}
+
+# Utility function to save a plot to the disk.
+my_save_plot <- function(name, plot_func, ...) {
+  # display plot
+  plot_func(...)
+  # save plot
+  filepath = filepath_png(name)
+  png(filepath)
+  plot_func(...)
+  dev.off() # COMMENT THIS LINE TO PREVENT PLOT SAVING
+}
+
 # generates bins for lm model homoscedascity  
 quantcut <- function(x, digits=6) { 
   cut(x, breaks=quantile(x), include.lowest=TRUE, dig.lab = digits) 
