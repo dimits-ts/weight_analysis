600d5bdd0f2fa48cc1eb1f6b7a0baed6ca62d528
Dimitris Tsirmpas <tsirbasdim@gmail.com>
2023-12-23 19:34:37 +0200

    Create part 2 of report

--- weightloss.r
+++ weightloss.r
@@ -1,49 +1,20 @@
+source("lib.r")
 library(nortest)
 library(car)
-source("lib.r")
-
+library(FSA)
 
 
 # IMPORT DATA
+
 data <- read.table("weightloss.txt", header=T)
 data$workout = factor(data$workout)
 data$diet = factor(data$diet)
 
 
-# Create dataset with dummy variables for factor levels
-
-# Create dummy variables for the 'diet' and 'workout' factors
-dummy_diet <- model.matrix(~ diet - 1, data=data)
-dummy_workout <- model.matrix(~ workout - 1, data = data)
-
-colnames(dummy_interactions) <- gsub("diet","",colnames(dummy_interactions))
-colnames(dummy_interactions) <- gsub("workout","",colnames(dummy_interactions))
-colnames(dummy_interactions) <- gsub(":","_",colnames(dummy_interactions))
-
-# Create dummy variables for the interactions
-dummy_interactions <- model.matrix(~ diet:workout - 1, data = data)
-colnames(dummy_interactions) <- gsub("diet","",colnames(dummy_interactions))
-colnames(dummy_interactions) <- gsub("workout","",colnames(dummy_interactions))
-colnames(dummy_interactions) <- gsub(":","_",colnames(dummy_interactions))
-
-# Combine all the dummy variables into a new dataframe
-dummy_data <- cbind(dummy_diet, dummy_workout, dummy_interactions)
-dummy_data <- cbind(dummy_data,data$loss)
-
-# Rename the columns to have better names
-colnames(dummy_data) <- c(levels(data$diet),
-                          levels(data$workout), 
-                          colnames(dummy_interactions),
-                          "loss")
-
-dummy_data <- as.data.frame(dummy_data)
-
-# View the resulting dataframe
-head(dummy_data)
-
-
 # ANALYSIS
 
+name = filepath_png("boxplot_2")
+png(name)
 par(mfrow=c(3,1), mar=c(7,5,1,1))
 boxplot(loss ~ workout, data=data)
 boxplot(loss ~ diet, data=data)
@@ -52,98 +23,147 @@ mtext("Weight loss depending on workout and diet",
       side = 3, 
       line = - 1, 
       outer = TRUE)
+dev.off()
 
+
+# ANOVA on workout
 anova <- aov(loss ~ workout, data=data)
 summary(anova)
 shapiro.test(anova$res)
 lillie.test(anova$res)
 leveneTest(loss ~ workout, data=data)
-pairwise.t.test(data$loss, data$workout, p.adjust.method = "holm")
 
+anova <- kruskal.test(loss ~ workout, data=data)
+anova
+summary(lm(loss ~ workout, data=data))
 
+# ANOVA on diet
 anova <- aov(loss ~  diet, data=data)
 summary(anova)
 shapiro.test(anova$res)
 lillie.test(anova$res)
 leveneTest(loss ~ diet, data=data)
-pairwise.t.test(data$loss, data$diet, p.adjust.method = "holm")
+
+anova <- kruskal.test(loss ~  diet, data=data)
+anova
+summary(lm(loss ~ diet, data=data))
+dunnTest(loss ~ diet, data=data)
 
 
-signif_data=dummy_data
-anova <- aov(loss ~  D1+D2+D3+D4, data=dummy_data)
+# re-run ANOVA on diet
+signif_data <- dummy_data
+anova <- aov(loss ~ D2+D3+D4, data=dummy_data)
 summary(anova)
 shapiro.test(anova$res)
 lillie.test(anova$res)
 leveneTest(loss ~ diet, data=data)
-#TukeyHSD(anova)
 
-# use LM instead of anova since they are equivalent and we need
-# lm in order to compare models later on
-anova_main <- lm(loss ~ W1+W2+W3+D1+D2+D3, data=dummy_data)
+excluded_data = data[data$diet != "D1",]
+anova <- kruskal.test(loss ~ diet, data=excluded_data)
+anova
+summary(lm(loss ~ diet, data=excluded_data))
+
+
+#TWO-WAY ON MAIN EFFECTS
+
+anova_main <- aov(loss ~ workout + diet, data=data)
 summary(anova_main)
+
 shapiro.test(anova_main$res)
 lillie.test(anova_main$res)
-
-qfits <- quantcut(anova_main$fit)
 # Homogeneity test.
+qfits <- quantcut(anova_main$fit)
 leveneTest(anova_main$res, qfits)
 bartlett.test(anova_main$res, qfits)
 
+vif(lm(loss ~ workout + diet, data=data))
+stargazer(lm(loss ~ workout + diet, data=data),
+          type="latex", 
+          title="Two-Way ANOVA between weight loss and main effects", 
+          ci=T, 
+          label="tab::main_2",
+          df=T,
+          out=filepath("main_2.tex"), 
+          report=('vc*p'), 
+          no.space=TRUE)
 
-anova_full <- lm(loss ~ ., data=dummy_data)
-summary(anova_full)
-shapiro.test(anova_full$res)
-lillie.test(anova_full$res)
-qfits <- quantcut(anova_full$fit)
+TukeyHSD(anova_main)
+
+
+# FIXED TWO WAY ON MAIN EFFECTS
+excluded_data = data[(data$workout != "W3") & (data$diet !="D1"),]
+anova_fixed <- aov(loss ~ workout + diet, data=excluded_data)
+shapiro.test(anova_fixed$res)
+lillie.test(anova_fixed$res)
 # Homogeneity test.
-leveneTest(anova_full$res, qfits)
-bartlett.test(anova_full$res, qfits)
+qfits <- quantcut(anova_fixed$fit)
+leveneTest(anova_fixed$res, qfits)
+bartlett.test(anova_fixed$res, qfits)
+
+summary.lm(anova_fixed)
 
 
-fullModel = anova_full 
-nullModel = lm(loss ~ 1, data = data) 
+#TWO-WAY WITH INTERACTIONS
+full_model = aov(loss ~ diet*workout, data=data)
+summary.lm(full_model)
+
+shapiro.test(full_model$res)
+lillie.test(full_model$res)
+qfits <- quantcut(full_model$fit)
+# Homogeneity test.
+leveneTest(full_model$res, qfits)
+bartlett.test(full_model$res, qfits)
+vif(full_model, type = "predictor")
+
+stargazer(lm(loss ~ diet*workout, data=data),
+          type="latex", 
+          title="Two-Way ANOVA between weight loss and main effects 
+          including interactions", 
+          ci=T, 
+          label="tab::full_2",
+          df=T,
+          out=filepath("full_2.tex"), 
+          report=('vc*p'), 
+          no.space=TRUE)
+
+
+# STEPWISE SELECTION
+
+# Use lm instead of aov for model selection
+# remember that ANOVA models are a special case of LM models 
+# and are thus equivalent
+start_model = lm(loss ~ diet*workout, data=data)
+null_model = lm(loss ~ 1, data = data) 
 optimal_anova_model = step(
-  anova_full,
-  direction = 'both', 
-  scope = list(upper = fullModel, 
-               lower = nullModel), 
-  trace = 0) 
+  start_model,
+        direction = 'both', 
+        scope = list(upper = start_model, 
+                     lower = null_model), 
+        trace = 0) 
 
 # View optimal model stats
 summary(optimal_anova_model)
+# same model, we do not need to check assumptions
 
-# Compare BIC values
-AIC(anova_full)
-AIC(optimal_anova_model)
-
-par(mfrow=c(1,1))
-coefs = optimal_anova_model$coefficients
-plot(x=dummy_data$loss, 
-     ylab = "Weight loss per day (calories)", 
-     xlab="",  
-     xaxt='n')
-
-colors = grDevices::colors()[grep('gr(a|e)y', grDevices::colors(), invert = T)]
-selected_colors = colors[c(12, 34, 256, 89, 98, 122, 385, 283)]
-ltypes = c("dotted", "dotted", "dotted", "longdash", "longdash", "dashed", "dashed", "solid")
-
-abline(h=coefs["(Intercept)"] + coefs["D1"], col=selected_colors[1], lty=ltypes[1])
-abline(h=coefs["(Intercept)"] + coefs["D2"], col=selected_colors[2], lty=ltypes[2])
-abline(h=coefs["(Intercept)"] + coefs["D3"], col=selected_colors[3], lty=ltypes[3])
-abline(h=coefs["(Intercept)"] + coefs["W1"], col=selected_colors[4], lty=ltypes[4])
-abline(h=coefs["(Intercept)"] + coefs["W2"], col=selected_colors[5], lty=ltypes[5])
-abline(h=coefs["(Intercept)"] + coefs["D2_W1"], col=selected_colors[6], lty=ltypes[6])
-abline(h=coefs["(Intercept)"] + coefs["D2_W2"], col=selected_colors[7], lty=ltypes[7])
-abline(h=coefs["(Intercept)"], col=selected_colors[8], lty=ltypes[8])
-
-legend("bottomleft", 
-       legend=c("Diet 1", "Diet 2", "Diet 3", 
-                "Weight 1", "Weight 2", 
-                "Diet 2 x Weight 1", "Diet 2 x Weight 2", "Overall Mean"), 
-       lty=ltypes,
-       col=selected_colors)
+# PLOT
+name = filepath_png("interaction_2")
+png(name)
+interaction.plot(data$workout, 
+                 data$diet, 
+                 data$loss, 
+                 xlab="Workout Category",
+                 ylab="Average weight loss (calories per day)",
+                 trace.label = "Diet Category",
+                 col=1:4, 
+                 lwd=3, 
+                 type='l', 
+                 cex=2, 
+                 pch=16, 
+                 lty=1:4, 
+                 bty='l')
 title("Weight loss by diet and weight")
+dev.off()
 
-
-anova(nullModel, anova_main)
-anova(nullModel, anova_full)
+# MODEL COMPARISONS
+anova(null_model, anova_main)
+anova(null_model, anova_full)
